e7427c7705828cea0365337b786e91806a2ac1ce
sadra <lnxpylnxpy@gmail.com>
2022-03-16 22:49:32 +0330

    Snippet simple class created

---
+++ utils.py
@@ -0,0 +1,31 @@
+from constants import PUSH_SNIPPET_URL
+
+
+class Snippet:
+    '''
+    A class to create a snippet object and
+    perform the push action to the POST API
+    `more information. https://codehub.pythonanywhere.com/api/v1/docs#operation/snippet_create`_
+    '''
+
+    def __init__(self, title, body, lang, description=''):
+        self.title = title
+        self.body = body
+        self.lang = lang
+        self.description = description
+
+    def push(self):
+        from requests import post
+        
+        data = {
+            'title': self.title,
+            'description': self.description,
+            'body': self.body,
+            'lang': self.lang,
+        }
+
+        r = post(
+            PUSH_SNIPPET_URL,
+            data=data,
+        )
+        return r
